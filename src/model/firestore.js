89bedc2d333e40e9f30cd6b95b579f44c165d935
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-14 17:47:36 -0300

    feat: Initialize firestore with json credentials

--- src/model/firestore.js
+++ src/model/firestore.js
@@ -2,3 +2,10 @@ import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
 import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
 import * as serviceAccount from '../../serviceAccountKey.json' assert { type: "json" };
 
+initializeApp({
+  credential: cert(
+    {
+    }
+  )
+});
+
