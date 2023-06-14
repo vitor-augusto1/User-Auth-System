12efd6da606fb87b17af5d4361aa5566e94fe1c7
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-14 18:30:09 -0300

    feat: Define a new function to create a new user document in firestore

---
+++ src/model/User/UserCreation.ts
@@ -0,0 +1,13 @@
+import { UserSchema } from "../../schemas/user";
+import { firestoreInstance } from "../firestore";
+
+async function createNewUserDocument(newUser: UserSchema): Promise<void> {
+  const userEmail = newUser.email;
+  const documentReference = firestoreInstance.collection('users').doc(userEmail);
+  await documentReference.set({
+    userName: newUser.userName,
+    email: userEmail,
+    password: " ", //Encrypt user's password before storing it
+    userRole: newUser.userRole,
+  });
+}
