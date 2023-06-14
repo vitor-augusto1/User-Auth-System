77c2e91a6ecf71bfe0e6ccf947971c2463d6c98b
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-14 17:48:13 -0300

    document creation example

--- src/model/firestore.js
+++ src/model/firestore.js
@@ -11,3 +11,10 @@ initializeApp({
 
 export const firestoreInstance = getFirestore();
 
+//const docRef = db.collection('users').doc('alovelace');
+//
+//await docRef.set({
+//  first: 'Ada',
+//  last: 'Lovelace',
+//  born: 1815
+//});
