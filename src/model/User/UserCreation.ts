a7ebe0196ed3b6cd1e9be2313cb4a34442965761
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-19 11:38:06 -0300

    feat: Create a new function to check if the user is providing a valid password

--- src/model/User/UserCreation.ts
+++ src/model/User/UserCreation.ts
@@ -16,6 +16,12 @@ async function encryptUserPassword(userPassword: string): Promise<string | boole
 }
 
 
+async function userHasAvalidPassword(userPassword: string, hash: string): Promise<boolean> {
+  const isUserPassowordValid: boolean = await bcrypt.compare(userPassword, hash);
+  return isUserPassowordValid;
+}
+
+
   const userEmail = newUser.email;
   const documentReference = firestoreInstance.collection('users').doc(userEmail);
   await documentReference.set({
