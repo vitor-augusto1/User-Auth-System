dfba3adf4b1976cc9287910bd03d2db046a9a595
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-19 12:48:15 -0300

    feat: Initialize the server

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+import userRegistrationRouter from "./routes/user-registration.router";
+
+const app: Express = express();
+const serverPort: number = 3333;
+
+app.use(userRegistrationRouter);
+
+app.listen(serverPort, () => {
+  console.log(`Server is running on port ${serverPort}`);
+})
