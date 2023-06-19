709fbb384bc830665a22d205ab0ce9a118659f8e
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-19 11:37:22 -0300

    feat: Return true or false depending on the hashing process answer

--- src/model/User/UserCreation.ts
+++ src/model/User/UserCreation.ts
@@ -5,6 +5,14 @@ import bcrypt from "bcrypt";
 
 async function encryptUserPassword(userPassword: string): Promise<string | boolean> {
   const saltRounds: number = 20;
+  try {
+    const hashedPassword: string = await bcrypt.hash(userPassword, saltRounds);
+    console.log(`Hashed password: ${hashedPassword}`);
+    return hashedPassword;
+  } catch (error) {
+    console.log(`Error hashing the password: ${error}`);
+    return false;
+  }
 }
 
 
