4839b9260d42031db454e6702384acc1447c1756
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-23 13:06:46 -0300

    feat: Create new custom type for the firestore document being returned

--- src/model/User/findUser.ts
+++ src/model/User/findUser.ts
@@ -1,6 +1,7 @@
 import { firestoreInstance } from "../firestore";
 
 
+type FirestoreDocument = FirebaseFirestore.DocumentData | undefined;
 
 
 export async function findUserbyEmail(email: string): Promise<boolean | FirestoreDocument> {
