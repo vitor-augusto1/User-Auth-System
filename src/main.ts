ce7ff6a8f2f6048c1dfa411d97e43cab4d306cbd
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-23 09:52:59 -0300

    feat: Add index route

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use(userRegistrationRouter);
 
+app.get('/', (req, res) => {
+  return res.status(200).json({ hello: "World" });
+})
+
 app.listen(serverPort, () => {
   console.log(`Server is running on port ${serverPort}`);
 })
