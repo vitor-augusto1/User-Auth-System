9783a4abebf0fe2929d91d32c0ab1d80e4c874f7
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-23 13:07:02 -0300

    feat: Return the user's document

--- src/model/User/findUser.ts
+++ src/model/User/findUser.ts
@@ -5,4 +5,15 @@ type FirestoreDocument = FirebaseFirestore.DocumentData | undefined;
 
 
 export async function findUserbyEmail(email: string): Promise<boolean | FirestoreDocument> {
+  const docSnapshot = await firestoreInstance
+                              .collection('users')
+                              .doc(email).get();
+  const documentDoesNotExist: boolean = !docSnapshot.exists;
+  if (documentDoesNotExist) {
+    console.log("User does not exists");
+    return false;
+  }
+  const userData: FirestoreDocument = docSnapshot.data();
+  console.log(`User found: ${userData}`);
+  return userData;
 }
