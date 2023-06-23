8c8f1b81ea0249084ecb1a44d9481d6e0ffa88fa
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-23 14:16:49 -0300

    feat: Add new login route

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import userRegistrationRouter from "./routes/user-registration.router";
+import userLoginRouter from "./routes/user-login.router";
 
 const app: Express = express();
 const serverPort: number = 3333;
@@ -7,6 +8,7 @@ const serverPort: number = 3333;
 app.use(express.json());
 
 app.use(userRegistrationRouter);
+app.use(userLoginRouter);
 
 app.get('/', (req, res) => {
   return res.status(200).json({ hello: "World" });
