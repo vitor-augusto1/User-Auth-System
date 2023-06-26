f339e153cceef2614dae21bc78c90778b8f50bb1
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-26 08:31:13 -0300

    feat: Add new route for the user profile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import userRegistrationRouter from "./routes/user-registration.router";
 import userLoginRouter from "./routes/user-login.router";
+import userProfileRouter from "./routes/user-profile.router";
 
 const app: Express = express();
 const serverPort: number = 3333;
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use(userRegistrationRouter);
 app.use(userLoginRouter);
+app.use(userProfileRouter);
 
 app.get('/', (req, res) => {
   return res.status(200).json({ hello: "World" });
